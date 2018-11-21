06ae88f8bf8c45c96ed4ece64d047ae3b5833bfe
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-11-21 18:29:55 +0100

    Added basic dependencies

---
+++ src/main/java/pl/coderslab/app/SpringDiApplication.java
@@ -0,0 +1,9 @@
+package pl.coderslab.app;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDiApplication {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+    }
+}
